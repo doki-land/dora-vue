50038e41b46a7a9828ef6eb7488322af5a20e94a
Aster <galaster@foxmail.com>
2025-03-17 02:25:42 +0800

    Convert to workspace

---
+++ projects/dora-core/vite.config.ts
@@ -0,0 +1,38 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
+
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'DoraCore',
+            fileName: (format) => `dora-core.${format}.js`
+        },
+        rollupOptions: {
+            external: ['vue', 'vue-router', 'pinia', '@vueuse/core', 'fluent-vue', '@fluent/bundle'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                    'vue-router': 'VueRouter',
+                    pinia: 'Pinia',
+                    '@vueuse/core': 'VueUse',
+                    'fluent-vue': 'FluentVue',
+                    '@fluent/bundle': 'FluentBundle'
+                }
+            }
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "@/styles/_variables.scss";'
+            }
+        }
+    }
